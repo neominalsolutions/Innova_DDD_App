e1674ec656853fc32d330f9e7cdc29ec921953fa
neominal.solutions <neominal.solutions@gmail.com>
2024-10-31 15:12:50 +0300

    EFPurchaseQuoteRepo implemente edildi.

---
+++ src/PO.API/Data/EFPurchaseQuoteRepo.cs
@@ -0,0 +1,11 @@
+﻿using PO.DomainLayer.Aggregates.PQ;
+
+namespace PO.API.Data
+{
+  public class EFPurchaseQuoteRepo : EFRepository<PoDbContext, PurchaseQuote>, IPurchaseQuoteRepository
+  {
+    public EFPurchaseQuoteRepo(PoDbContext dbContext) : base(dbContext)
+    {
+    }
+  }
+}
