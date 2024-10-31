de9241bf890a2ba1fdc3e7ba8d022b1b04c65641
neominal.solutions <neominal.solutions@gmail.com>
2024-10-31 14:10:23 +0300

    RejectAllPurchaseQuoteHandler implemente ettik.

--- src/PO.DomainLayer/Handlers/RejectAllPurchaseQuoteHandler.cs
+++ src/PO.DomainLayer/Handlers/RejectAllPurchaseQuoteHandler.cs
@@ -1,5 +1,6 @@
 ﻿using MediatR;
 using PO.DomainLayer.Aggregates.PO;
+using PO.DomainLayer.Aggregates.PQ;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,8 +11,23 @@ namespace PO.DomainLayer.Handlers
 {
   public class RejectAllPurchaseQuoteHandler : INotificationHandler<PurchaseOrderCanceled>
   {
+    private readonly IPurchaseQuoteRepository purchaseQuoteRepository;
+
+    public RejectAllPurchaseQuoteHandler(IPurchaseQuoteRepository purchaseQuoteRepository)
+    {
+      this.purchaseQuoteRepository = purchaseQuoteRepository;
+    }
+
     public Task Handle(PurchaseOrderCanceled notification, CancellationToken cancellationToken)
     {
+      var allPurchaseQuotes = this.purchaseQuoteRepository.Find(x => x.PurchaseRequestId == notification.PurchaseRequestId);
+
+      allPurchaseQuotes.ToList().ForEach((item) =>
+      {
+        item.OnReject();
+        this.purchaseQuoteRepository.Update(item);
+      });
+
       return Console.Out.WriteLineAsync($"{notification.PurchaseRequestId} ait tüm quoteları rejected yap \n");
     }
   }
