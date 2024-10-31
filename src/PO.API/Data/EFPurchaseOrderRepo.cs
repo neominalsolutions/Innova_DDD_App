9b1059fe2c5662dec9a91057c83f71d3ca4e1fff
neominal.solutions <neominal.solutions@gmail.com>
2024-10-31 15:13:51 +0300

    EFPurchaseOrderRepo

---
+++ src/PO.API/Data/EFPurchaseOrderRepo.cs
@@ -0,0 +1,11 @@
+﻿using PO.DomainLayer.Aggregates.PO;
+
+namespace PO.API.Data
+{
+  public class EFPurchaseOrderRepo : EFRepository<PoDbContext, PurchaseOrder>, IPurchaseOrderRepository
+  {
+    public EFPurchaseOrderRepo(PoDbContext dbContext) : base(dbContext)
+    {
+    }
+  }
+}
