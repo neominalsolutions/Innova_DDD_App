831ae1fb24e06d57adf5edbef0bf839834c1fcb1
neominal.solutions <neominal.solutions@gmail.com>
2024-10-31 15:11:43 +0300

    EF PurchaseRequest Repo

---
+++ src/PO.API/Data/EFPurchaseRequestRepo.cs
@@ -0,0 +1,12 @@
+﻿using PO.DomainLayer.Aggregates.PR;
+
+namespace PO.API.Data
+{
+  // Data katmanı ile Domain katmanının EF üzerinden haberleşmesi için Adapter sınıfı yazdık.
+  public class EFPurchaseRequestRepo : EFRepository<PoDbContext, PurchaseRequest>, IPurchaseRequestRepository
+  {
+    public EFPurchaseRequestRepo(PoDbContext dbContext) : base(dbContext)
+    {
+    }
+  }
+}
